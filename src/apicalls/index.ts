50e7eb414723dedb839ee00c9718002679dbbdb1
hanaddaahir124@gmail.com <hanaddaahir124@gmail.com>
2024-06-24 02:54:59 +0300

    updated client base url

--- src/apicalls/index.ts
+++ src/apicalls/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "https://expense-tracker-api-3puk.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
